27406f5c29fbb3d5d389dd7262a07d69c8a82302
Norbert Pillmayer <norbert@pillmayer.com>
2020-12-04 22:56:04 +0100

    Initial commit

---
+++ lr/earley/debug.go
@@ -0,0 +1,39 @@
+package earley
+
+import (
+	"bytes"
+
+	"github.com/npillmayer/gorgo/lr"
+	"github.com/npillmayer/gorgo/lr/iteratable"
+)
+
+func dumpState(states []*iteratable.Set, stateno uint64) {
+	T().Debugf("--- State %04d ------------------------------------", stateno)
+	S := states[stateno]
+	n := 1
+	S.IterateOnce()
+	for S.Next() {
+		item := S.Item().(lr.Item)
+		T().Debugf("[%2d] %s", n, item)
+		n++
+	}
+}
+
+func itemSetString(S *iteratable.Set) string {
+	var b bytes.Buffer
+	b.WriteString("{")
+	S.IterateOnce()
+	first := true
+	for S.Next() {
+		item := S.Item().(lr.Item)
+		if first {
+			b.WriteString(" ")
+			first = false
+		} else {
+			b.WriteString(", ")
+		}
+		b.WriteString(item.String())
+	}
+	b.WriteString(" }")
+	return b.String()
+}
