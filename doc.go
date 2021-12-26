0da272467db45fcf5a59d3f3eaa1e1fc519b8e68
Norbert Pillmayer <norbert@pillmayer.com>
2021-12-26 19:10:46 +0100

    Documentation update

--- doc.go
+++ doc.go
@@ -1,7 +1,21 @@
 /*
 Package gorgo is an LR parsing toolbox.
+
 GoRGO strives to be a smart and lightweight tool to generate
 interpreters for DSLs.
+It focusses on LR-parsing and parsing of ambiguous grammars. Package structure is
+as follows:
+
+■ lr: Package lr implements LR parsers, together with supporting data structures like
+DAG-structured stacks and shared packed parse forests.
+
+■ terex: Package terex implements term rewriting, based on a Lisp-like data-structure to
+build homogenous abstract syntax trees.
+
+■ runtime: Package runtime provides some unsophisticated supporting data types for interpreter
+runtimes.
+
+The base package contiains data types which are used throughout all the other packages.
 
 License
 
